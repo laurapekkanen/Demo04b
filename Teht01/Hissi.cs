15208bf34157ec716a1a9c980e266a1635cbba84
Laura Pekkanen <K8966@student.jamk.fi>
2017-02-02 15:23:27 +0200

    Elevator made

---
+++ Teht01/Hissi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht01
+{
+    class Hissi
+    {
+        private readonly int minFloor = 1;
+        private readonly int maxFloor = 5;
+
+        private int floor = 1;
+
+        public int Floor
+        {
+            get
+            {
+                return floor;
+            }
+            set
+            {
+                if (value >= minFloor && value <= maxFloor) floor = value;
+                else
+                {
+                    Console.WriteLine("Wrong floor, choose another.");
+                }
+            }
+        }
+    }
+}
--- Teht01/Program.cs
+++ Teht01/Program.cs
@@ -10,6 +10,15 @@ namespace Teht01
     {
         static void Main(string[] args)
         {
+            Hissi hissi = new Hissi();
+
+            do
+            {
+                Console.WriteLine("Elevator is now on floor " + hissi.Floor);
+                Console.WriteLine("Give floor number (1-5) > ");
+                string line = Console.ReadLine();
+                hissi.Floor = int.Parse(line);
+            } while (hissi.Floor != 0);
         }
     }
 }
